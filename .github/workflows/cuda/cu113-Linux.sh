7dfdb31c1333efd40b21d7ce83cd4d335d1543e8
Elie-Alban LESCOUT <elie-alban.lescout@ensg.eu>
2022-05-31 16:59:38 +0200

    removed testing and update to ubuntu 20.04

--- .github/workflows/cuda/cu113-Linux.sh
+++ .github/workflows/cuda/cu113-Linux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-OS=ubuntu1804
+OS=ubuntu2004
 
 wget -nv https://developer.download.nvidia.com/compute/cuda/repos/${OS}/x86_64/cuda-${OS}.pin
 sudo mv cuda-${OS}.pin /etc/apt/preferences.d/cuda-repository-pin-600
@@ -17,7 +17,7 @@ rm -f https://developer.download.nvidia.com/compute/cuda/11.3.0/local_installers
 
 # Testing CUDA install
 nvcc -V
-cd ~/
-sudo apt-get install cuda-samples-11-3 -y
-cd /usr/local/cuda-11.3/samples
-make
+#cd ~/
+#sudo apt-get install cuda-samples-11-3 -y
+#cd /usr/local/cuda-11.3/samples
+# make
