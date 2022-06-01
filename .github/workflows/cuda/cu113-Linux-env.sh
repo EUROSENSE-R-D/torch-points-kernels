6970188b81112b10bb5437f38ad46101368744b4
Elie-Alban LESCOUT <elie-alban.lescout@ensg.eu>
2022-06-01 16:26:24 +0200

    checking folder existence

--- .github/workflows/cuda/cu113-Linux-env.sh
+++ .github/workflows/cuda/cu113-Linux-env.sh
@@ -5,6 +5,10 @@ LD_LIBRARY_PATH=${CUDA_HOME}/lib64:${LD_LIBRARY_PATH}
 PATH=${CUDA_HOME}/bin:${PATH}
 
 echo "*** ENV VARIABLES: $CUDA_HOME, $LD_LIBRARY_PATH, $PATH"
+ls /usr/local/cuda
+ls $CUDA_HOME
+
+
 
 export FORCE_CUDA=1
 export TORCH_CUDA_ARCH_LIST="3.5;5.0+PTX;6.0;7.0;7.5;8.0;8.6"
